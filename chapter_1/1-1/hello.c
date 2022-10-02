7aca6034cf8382d9c074b12a3d816ea0720d6a2d
JeanPiette1 <benjamin.schweitzer1701@gmail.com>
2022-10-02 23:55:49 +0200

    hello_world

---
+++ chapter_1/1-1/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // include library wich contain printf function
+
+int main(void)  // (void) because no argument needed
+{
+    printf("hellow, world !\n");
+    return (0); // return (0) because function main return a int
+}
